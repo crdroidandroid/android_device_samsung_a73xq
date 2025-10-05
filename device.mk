d282511f6ee1f0619683a96430f605655422a0c6
Bruno Martins <bgcngm@gmail.com>
2025-10-05 12:32:10 -0300

    a73xq: Move UDFPS config to new soong namespace Change-Id: Id5eb8f0f0b4962655c593a6ae5c87c4226174e3c

--- device.mk
+++ device.mk
@@ -53,5 +53,5 @@ PRODUCT_PACKAGES += \
     sensors.a73xq
 
 # UDFPS
-$(call soong_config_set,samsung_udfps,udfps_zorder,0x20000000u)
+$(call soong_config_set,samsungUdfpsVars,udfps_zorder,0x20000000u)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
